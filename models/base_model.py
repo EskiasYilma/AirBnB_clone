5db1b6a8c204647fc762ba6d3b9da0f7bda68c07
exuszee <exuszee@gmail.com>
2023-03-11 12:52:41 +0300

    All models and console

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,89 @@
 #!/usr/bin/python3
-
 """
 Base Model Docstring
+
 """
 
 import uuid
+from datetime import datetime
+import os
+import models
+
 
 class BaseModel:
     """
-    TODO
+    BaseModel Class Docstring
+
+    Defines all common attributes/methods for other classes
     """
-    def __init__(self):
-        pass
+    def __init__(self, *args, **kwargs):
+        """
+        init Function Docstring
+
+        Class Constructor
+        id: string - assignes a unique uuid when an\
+        instance is created
+        created_at: datetime - assign with the current\
+        datetime when an instance is created
+        updated_at: datetime - assign with the current\
+        datetime when an instance is created and it will\
+        be updated every time you change
+        """
+        if kwargs:
+            id_exists = 0
+            created_at_exists = 0
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        if key == "created_at":
+                            created_at_exists = 1
+                        if not isinstance(value, datetime):
+                            value = datetime.strptime(
+                                                    value,
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+                    if key == 'id':
+                        id_exists = 1
+                    setattr(self, key, value)
+            if id_exists == 0:
+                self.id = str(uuid.uuid4())
+            if created_at_exists == 0:
+                self.created_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+
+    def __str__(self):
+        """
+        __str__ Function Docstring
+
+        Return: [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id,
+                                     self.__dict__)
+
+    def save(self):
+        """
+        save Function Docstring
+
+        Return: Updates the public instance attribute\
+        updated_at with the current datetime
+        """
+        self.__dict__["updated_at"] = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        to_dict Function Docstring
+
+        Return: the dictionary representation of BaseModel
+        """
+        res = self.__dict__.copy()
+        res['__class__'] = self.__class__.__name__
+        dates = {'created_at': self.created_at}
+        for i, j in dates.items():
+            res[str(i)] = j.isoformat()
+        if ("updated_at" in res):
+            res["updated_at"] = self.updated_at.isoformat()
+        return res
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,102 @@
 #!/usr/bin/python3
 """
-Module Docstring
+FileStorage Module Docstring
+
+Private class attributes:
+
+    __file_path: string - path to the JSON file (ex: file.json)
+    __objects: dictionary - empty but will store all objects by \
+    <class name>.id (ex: to store a BaseModel object with \
+    id=12121212, the key will be BaseModel.12121212)
+
+Public instance methods:
+
+    all(self): returns the dictionary __objects
+
+    new(self, obj): sets in __objects the obj with key <obj class name>.id
+
+    save(self): serializes __objects to the JSON file (path: __file_path)
+
+    reload(self): deserializes the JSON file to __objects (only if the JSON\
+    file (__file_path) exists ; otherwise, do nothing. If the file doesn’t\
+    exist, no exception should be raised)
+
 """
+
+import os
+import json
+from datetime import datetime
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+
+
+class FileStorage:
+    """
+    FileStorage Class Docstring
+
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def __init__(self):
+        """
+
+        """
+        self.__models = {"BaseModel": BaseModel, "User": User,
+                         "State": State, "Review": Review, "Place": Place,
+                         "City": City, "Amenity": Amenity}
+
+    def all(self):
+        """
+        all Function Docstring
+        -----------------------
+        Returns the dictionary __objects.
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        new Function Docstring
+        -----------------------
+        Sets in __objects the obj with key <obj class name>.id.
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        save Function Docstring
+        -----------------------
+        Serializes __objects to the JSON file (path: __file_path).
+        """
+        obj_dict = {}
+        for i, j in self.__objects.items():
+            obj_dict[i] = j.to_dict()
+        with open(self.__file_path, 'w', encoding='utf-8') as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """
+        reload Function Docstring
+        -----------------------
+        Deserializes the JSON file to __objects.
+        """
+
+        self.__objects = {}
+        try:
+            with open(self.__file_path,
+                      mode="r", encoding="utf-8") as f:
+                json_file = json.load(f)
+        except Exception as e:
+            return
+
+        for key, value in json_file.items():
+            cls = value.pop("__class__", None)
+            if cls not in self.__models.keys():
+                continue
+            self.__objects[key] = self.__models[cls](**value)
