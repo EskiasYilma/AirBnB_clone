3b10ca851ead52e4424158311f174cc416821876
exuszee <exuszee@gmail.com>
2023-03-11 16:53:16 +0300

    File storage fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -50,6 +50,7 @@ class FileStorage:
         self.__models = {"BaseModel": BaseModel, "User": User,
                          "State": State, "Review": Review, "Place": Place,
                          "City": City, "Amenity": Amenity}
+        self.reload()
 
     def all(self):
         """
@@ -65,8 +66,9 @@ class FileStorage:
         -----------------------
         Sets in __objects the obj with key <obj class name>.id.
         """
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
         """
@@ -90,13 +92,14 @@ class FileStorage:
         self.__objects = {}
         try:
             with open(self.__file_path,
-                      mode="r", encoding="utf-8") as f:
+                      mode="r+", encoding="utf-8") as f:
                 json_file = json.load(f)
+                for key, value in json_file.items():
+                    cls = value.pop("__class__", None)
+                    if cls not in self.__models.keys():
+                        continue
+                    self.__objects[key] = self.__models[cls](**value)
         except Exception as e:
-            return
+            pass
 
-        for key, value in json_file.items():
-            cls = value.pop("__class__", None)
-            if cls not in self.__models.keys():
-                continue
-            self.__objects[key] = self.__models[cls](**value)
+
