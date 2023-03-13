fa36f8e64d4bdac3a9df499aae1d614ecfebf797
exuszee <exuszee@gmail.com>
2023-03-13 12:31:16 +0300

    test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -56,95 +56,95 @@ class TestConsole(unittest.TestCase):
 
     def test_do_EOF(self):
         """test_do_EOF Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.assertTrue(self.console.do_EOF(""))
 
     def test_emptyline(self):
         """test_emptyline Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.assertIsNone(self.console.emptyline())
 
     def test_do_create_error(self):
         """test_do_create_error Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_create("")
             self.assertEqual("** class name missing **\n",
-                             self.mock_stdout.getvalue())
+                             f.getvalue())
 
     def test_do_create(self):
         """test_do_create Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_create("BaseModel")
-        output = self.mock_stdout.getvalue().strip()
-        with patch("sys.stdout", self.mock_stdout):
+        output = f.getvalue().strip()
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_show("BaseModel {}".format(output))
-        output2 = self.mock_stdout.getvalue().strip()
+        output2 = f.getvalue().strip()
 
         self.assertTrue(output in output2)
 
     def test_do_show_instance_not_found(self):
         """test_do_show_instance_not_found Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_show("BaseModel 1234")
             self.assertEqual("** no instance found **\n",
-                             self.mock_stdout.getvalue())
+                             f.getvalue())
 
     def test_do_show_no_class(self):
         """test_do_show_no_class Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_show("")
             self.assertEqual("** class name missing **\n",
-                             self.mock_stdout.getvalue())
+                             f.getvalue())
 
     def test_do_show_id_missing(self):
         """test_do_show_id_missing Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_show("BaseModel")
             self.assertEqual("** instance id missing **\n",
-                             self.mock_stdout.getvalue())
+                             f.getvalue())
 
     def test_do_show_class_doesnt_exist(self):
         """test_do_show_class_doesnt_exist Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_show("JustModel")
             self.assertEqual("** class doesn't exist **\n",
-                             self.mock_stdout.getvalue())
+                             f.getvalue())
 
     def test_do_show_correct(self):
         """test_do_show_correct Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_show("BaseModel \
                                   d42f98a5-71d3-4237-83f3-f480c9dc3c18")
             self.assertFalse("2023, 3, 8, 19, 30, 48, 436966"
-                             in self.mock_stdout.getvalue().strip())
+                             in f.getvalue().strip())
             # self.assertTrue('2023, 3, 8, 19, 30, 48, 436849'
-            #                 in self.mock_stdout.getvalue().strip())
+            #                 in f.getvalue().strip())
 
     def test_do_all(self):
         """test_do_all Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_all("")
             self.assertFalse("2023, 3, 8, 19, 30, 48, 436966"
-                             in self.mock_stdout.getvalue().strip())
+                             in f.getvalue().strip())
             self.assertTrue('2023, 3, 8, 19, 30, 48, 436849'
-                            in self.mock_stdout.getvalue().strip())
+                            in f.getvalue().strip())
 
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_create("BaseModel")
             self.console.do_all("BaseModel")
             self.assertIn(str(list(storage.all().values())[0]),
-                          self.mock_stdout.getvalue())
+                          f.getvalue())
 
     def test_all_error_class_doesnt_exist(self):
         """test_all_error_class_doesnt_exist Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_all("C")
-        self.assertEqual(self.mock_stdout.getvalue().strip(),
+        self.assertEqual(f.getvalue().strip(),
                          "** class doesn't exist **")
 
     def test_do_destroy_correct(self):
         """test_do_destroy_correct Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_create("BaseModel")
             all_objs = storage.all()
             obj_id = list(all_objs.values())[0].id
@@ -153,90 +153,90 @@ class TestConsole(unittest.TestCase):
 
     def test_destroy_error_class_doesnt_exist(self):
         """test_destroy_error_class_doesnt_exist Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_destroy("d3da85f2-499c-43cb-b33d-3d7935bc808c")
-        self.assertEqual(self.mock_stdout.getvalue().strip(),
+        self.assertEqual(f.getvalue().strip(),
                          "** class doesn't exist **")
 
     def test_destroy_error_class_missing(self):
         """test_destroy_error_class_missing Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_destroy("")
-        self.assertEqual(self.mock_stdout.getvalue().strip(),
+        self.assertEqual(f.getvalue().strip(),
                          "** class name missing **")
 
     def test_destroy_error_no_instance_found(self):
         """test_destroy_error_no_instance_found Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_destroy("BaseModel 1234")
-        self.assertEqual(self.mock_stdout.getvalue().strip(),
+        self.assertEqual(f.getvalue().strip(),
                          "** no instance found **")
 
     def test_destroy_error_id_instance_misssing(self):
         """test_destroy_error_id_instance_misssing Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_destroy("BaseModel")
-        self.assertEqual(self.mock_stdout.getvalue().strip(),
+        self.assertEqual(f.getvalue().strip(),
                          "** instance id missing **")
 
     def test_do_update_error(self):
         """test_do_update_error Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_update("BaseModel 1234")
             self.assertEqual("** no instance found **\n",
-                             self.mock_stdout.getvalue())
+                             f.getvalue())
 
     def test_update_error_class_doesnt_exist(self):
         """test_update_error_class_doesnt_exist Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_update("TEst")
-        self.assertEqual(self.mock_stdout.getvalue().strip(),
+        self.assertEqual(f.getvalue().strip(),
                          "** class doesn't exist **")
 
     def test_update_error_class_missing(self):
         """test_update_error_class_missing Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_update("")
-        self.assertEqual(self.mock_stdout.getvalue().strip(),
+        self.assertEqual(f.getvalue().strip(),
                          "** class name missing **")
 
     def test_update_error_no_instance_found(self):
         """test_update_error_no_instance_found Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_update("BaseModel 1234")
-        self.assertEqual(self.mock_stdout.getvalue().strip(),
+        self.assertEqual(f.getvalue().strip(),
                          "** no instance found **")
 
     def test_update_error_id_instance_misssing(self):
         """test_update_error_id_instance_misssing Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_update("BaseModel")
-        self.assertEqual(self.mock_stdout.getvalue().strip(),
+        self.assertEqual(f.getvalue().strip(),
                          "** instance id missing **")
 
     def test_do_update_attr_missing(self):
         """test_do_update_attr_missing Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_create("User")
             all_objs = storage.all()
             obj_id = list(all_objs.values())[0].id
             self.console.do_update("User " + obj_id)
-        self.assertEqual(self.mock_stdout.getvalue().strip().split("\n")[1],
+        self.assertEqual(f.getvalue().strip().split("\n")[1],
                          "** attribute name missing **")
 
     def test_do_update_attr_value_missing(self):
         """test_do_update_attr_value_missing Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_create("User")
             all_objs = storage.all()
             obj_id = list(all_objs.values())[0].id
             self.console.do_update("User " + obj_id + " first_name")
-        self.assertEqual(self.mock_stdout.getvalue().strip().split("\n")[1],
+        self.assertEqual(f.getvalue().strip().split("\n")[1],
                          "** value missing **")
 
     def test_do_update_correct(self):
         """test_do_update_correct Docstring"""
-        with patch("sys.stdout", self.mock_stdout):
+        with patch("sys.stdout", new=StringIO()) as f:
             self.console.do_create("BaseModel")
             all_objs = storage.all()
             obj_id = list(all_objs.values())[0].id
